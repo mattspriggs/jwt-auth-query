3bd0623ade71e3bd713be5e39bdd8b83aa33b0a0
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-15 10:48:52 +1300

    Auth0 setup

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="manaia-2023-matt"
+    clientId="tzc5jFSHtpcPMIpos1qyFJfPn9n4VTdE"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
