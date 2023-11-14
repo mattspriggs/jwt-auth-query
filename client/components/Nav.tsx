7205ce04817fafdcbfcabb66d669ee85d8b01c83
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-15 11:57:44 +1300

    Auth0 name showing

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -6,9 +6,7 @@ function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   const { logout, loginWithRedirect } = useAuth0()
   // TODO: replace placeholder user object with the one from auth0
-  const user = {
-    nickname: 'john.doe',
-  }
+  const { user } = useAuth0()
 
   const handleSignOut = () => {
     logout()
